4d7545c48a5ed59e716e08ebac31abea3045c715
chenkuifang <314287696@qq.com>
2017-12-16 11:20:41 +0800

    增加开发配置文件和生产配置文件

--- src/main/java/com/example/demo/MyApplication.java
+++ src/main/java/com/example/demo/MyApplication.java
@@ -1,16 +1,16 @@
 package com.example.demo;
 
+import com.example.demo.filter.XssFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-import com.example.demo.filter.XssFilter;
-
 @SpringBootApplication
 public class MyApplication {
 
     public static void main(String[] args) {
+        // 返回一个上下文
         SpringApplication.run(MyApplication.class, args);
     }
 
