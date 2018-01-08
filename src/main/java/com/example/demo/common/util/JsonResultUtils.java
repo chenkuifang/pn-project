35385f943b877dd045b984e3b49e5294b369e6f3
chenkuifang <314287696@qq.com>
2018-01-08 11:15:24 +0800

    删除不必要文件

--- src/main/java/com/example/demo/common/util/JsonResultUtils.java
+++ src/main/java/com/example/demo/common/util/JsonResultUtils.java
@@ -16,127 +16,113 @@ import java.util.List;
  */
 public final class JsonResultUtils {
 
-	private JsonResultUtils() {
-	}
+    private JsonResultUtils() {
+    }
 
-	/**
-	 * 返回普通json结果,只有code和msg属性值,返回："成功" 或者 "失败"
-	 *
-	 * @param flag
-	 *            判定是否成功的标志, >0 表示成功
-	 * @return
-	 */
-	public static JsonResult jsonResult(int flag) {
-		// 结果返回
-		if (flag > 0) {
-			return jsonResult(Constants.SUCCESS_CODE, Constants.SUCCESS_DESCRIPTION);
-		}
-		return jsonResult(Constants.FAIL_DESCRIPTION);
-	}
+    /**
+     * 返回普通json结果,只有code和msg属性值,返回："成功" 或者 "失败"
+     *
+     * @param flag 判定是否成功的标志, >0 表示成功
+     * @return
+     */
+    public static JsonResult jsonResult(int flag) {
+        // 结果返回
+        if (flag > 0) {
+            return jsonResult(Constants.SUCCESS_CODE, Constants.SUCCESS_DESCRIPTION);
+        }
+        return jsonResult(Constants.FAIL_DESCRIPTION);
+    }
 
-	/**
-	 * 返回成功数据，有效属性：code,msg,data
-	 *
-	 * @param data
-	 *            数据
-	 * @return
-	 */
-	public static JsonResult jsonResult(List<?> data) {
-		return jsonResult(data, 1);
-	}
+    /**
+     * 返回成功数据，有效属性：code,msg,data
+     *
+     * @param data 数据
+     * @return
+     */
+    public static JsonResult jsonResult(List<?> data) {
+        return jsonResult(data, 1);
+    }
 
-	/**
-	 * 返回普通json结果,有code,msg,data属性值，
-	 *
-	 * @param data
-	 *            数据
-	 * @param flag
-	 *            判定是否成功的标志, >0 表示成功
-	 * @return 返回："成功" 或者 "失败" + 数据
-	 */
-	public static JsonResult jsonResult(List<?> data, int flag) {
-		// 结果返回
-		if (flag > 0) {
-			return jsonResult(Constants.SUCCESS_CODE, Constants.SUCCESS_DESCRIPTION, data, 0);
-		}
+    /**
+     * 返回普通json结果,有code,msg,data属性值，
+     *
+     * @param data 数据
+     * @param flag 判定是否成功的标志, >0 表示成功
+     * @return 返回："成功" 或者 "失败" + 数据
+     */
+    public static JsonResult jsonResult(List<?> data, int flag) {
+        // 结果返回
+        if (flag > 0) {
+            return jsonResult(Constants.SUCCESS_CODE, Constants.SUCCESS_DESCRIPTION, data, 0);
+        }
 
-		return jsonResult(Constants.FAIL_CODE, Constants.FAIL_DESCRIPTION, data, 0);
-	}
+        return jsonResult(Constants.FAIL_CODE, Constants.FAIL_DESCRIPTION, data, 0);
+    }
 
-	/**
-	 * 返回错误信息结果,包含有效属性：code,msg
-	 *
-	 * @param msg
-	 *            返回的信息
-	 * @return
-	 */
-	public static JsonResult jsonResult(String msg) {
-		return jsonResultNoData(Constants.FAIL_CODE, msg);
-	}
+    /**
+     * 返回错误信息结果,包含有效属性：code,msg
+     *
+     * @param msg 返回的信息
+     * @return
+     */
+    public static JsonResult jsonResult(String msg) {
+        return jsonResultNoData(Constants.FAIL_CODE, msg);
+    }
 
-	/**
-	 * 根据参数返回普通json结果,包含有效属性：code,msg
-	 *
-	 * @param code
-	 *            自定义返回编码
-	 * @param msg
-	 *            自定义返回消息
-	 * @return
-	 */
-	public static JsonResult jsonResult(String code, String msg) {
-		return jsonResultNoData(code, msg);
+    /**
+     * 根据参数返回普通json结果,包含有效属性：code,msg
+     *
+     * @param code 自定义返回编码
+     * @param msg  自定义返回消息
+     * @return
+     */
+    public static JsonResult jsonResult(String code, String msg) {
+        return jsonResultNoData(code, msg);
 
-	}
+    }
 
-	/**
-	 * 返回需要翻页的json结果集,包含有效属性：code,msg,data,count
-	 *
-	 * @param data
-	 *            总数据
-	 * @param countPage
-	 *            总页数
-	 * @return
-	 */
-	public static JsonResult jsonPageResult(List<?> data, int countPage) {
-		return jsonResult(Constants.SUCCESS_CODE, Constants.SUCCESS_DESCRIPTION, data, countPage);
-	}
+    /**
+     * 返回需要翻页的json结果集,包含有效属性：code,msg,data,count
+     * 注意：由于该方法是提供layui table 使用 code 成功默认为0
+     *
+     * @param data      总数据
+     * @param countPage 总页数
+     * @return
+     */
+    public static JsonResult jsonPageResult(List<?> data, int countPage) {
+        return jsonResult("0", Constants.SUCCESS_DESCRIPTION, data, countPage);
+    }
 
-	/**
-	 * 需要数据返回时用(一般供列表使用)
-	 * 
-	 * @param code
-	 *            返回码
-	 * @param msg
-	 *            返回消息
-	 * @param data
-	 *            返回数据
-	 * @param countPage
-	 *            返回数据的总数
-	 * @return
-	 */
-	private static JsonResult jsonResult(String code, String msg, List<?> data, int countPage) {
-		JsonResult r = JsonResult.getInstance();
-		r.setCode(code);
-		r.setMsg(msg);
-		r.setCount(countPage);
-		r.setData(data);
-		return r;
-	}
+    /**
+     * 需要数据返回时用(一般供列表使用)
+     *
+     * @param code      返回码
+     * @param msg       返回消息
+     * @param data      返回数据
+     * @param countPage 返回数据的总数
+     * @return
+     */
+    private static JsonResult jsonResult(String code, String msg, List<?> data, int countPage) {
+        JsonResult r = JsonResult.getInstance();
+        r.setCode(code);
+        r.setMsg(msg);
+        r.setCount(countPage);
+        r.setData(data);
+        return r;
+    }
 
-	/**
-	 * 不需要数据返回时用
-	 * 
-	 * @param code
-	 *            返回码
-	 * @param msg
-	 *            返回消息
-	 * @return
-	 */
-	private static JsonResult jsonResultNoData(String code, String msg) {
-		JsonResult r = JsonResult.getInstance();
-		r.setCode(code);
-		r.setMsg(msg);
-		return r;
-	}
+    /**
+     * 不需要数据返回时用
+     *
+     * @param code 返回码
+     * @param msg  返回消息
+     * @return
+     */
+    private static JsonResult jsonResultNoData(String code, String msg) {
+        JsonResult r = JsonResult.getInstance();
+        r.setCode(code);
+        r.setMsg(msg);
+        return r;
+    }
 
 }
