6c5a6171760ae56e0562f9256ace0573d77ec07e
chenkuifang <314287696@qq.com>
2018-01-16 15:06:01 +0800

    增加日志输出界面，方便开发或维护

---
+++ src/main/java/com/example/demo/common/util/FileUtils.java
@@ -0,0 +1,165 @@
+package com.example.demo.common.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author QuiFar
+ * @version V1.0
+ * @Description: 文件读取工具类
+ */
+public class FileUtils {
+    private FileUtils() {
+    }
+
+    /**
+     * 以行为单位读取文件，常用于读面向行的格式化文件
+     */
+    public static List<String> readFileByLines(String fileName) {
+        List<String> data = new ArrayList<String>();
+
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        try {
+            System.out.println("以行为单位读取文件内容，一次读一整行：");
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                data.add(tempString);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+
+        return data;
+    }
+
+    /**
+     * 根据路径获取文件列表
+     *
+     * @param strPath 文件夹路径
+     * @return
+     */
+    public static List<File> getFileList(String strPath) {
+        List<File> filelist = new ArrayList<>();
+        File dir = new File(strPath);
+        File[] files = dir.listFiles(); // 该文件目录下文件全部放入数组
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                String fileName = files[i].getName();
+                if (files[i].isDirectory()) { // 判断是文件还是文件夹
+                    getFileList(files[i].getAbsolutePath()); // 获取文件绝对路径
+                } else if (fileName.endsWith("log")) { // 判断文件名是否以.avi结尾
+                    String strFileName = files[i].getAbsolutePath();
+                    System.out.println("---" + strFileName);
+                    filelist.add(files[i]);
+                } else {
+                    continue;
+                }
+            }
+
+        }
+        return filelist;
+    }
+
+    /**
+     * 根据路径获取文件名称列表
+     *
+     * @param strPath 文件夹路径
+     * @return
+     */
+    public static List<String> getFileNameList(String strPath) {
+        List<String> filelist = new ArrayList<>();
+        File dir = new File(strPath);
+        // 该文件目录下文件全部放入数组
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                // 判断是文件还是文件夹
+                if (files[i].isDirectory()) {
+                    // 获取文件绝对路径,递归
+                    getFileList(files[i].getAbsolutePath());
+                } else {
+                    String strFileName = files[i].getAbsolutePath();
+                    filelist.add(strFileName);
+                }
+            }
+        }
+
+        return filelist;
+    }
+
+    /**
+     * 根据路径获取文件绝对路径、名称列表
+     *
+     * @param strPath 文件夹路径
+     * @return
+     */
+    public static Map<String, String> getFileNameLists(String strPath) {
+        Map<String, String> map = new HashMap<String, String>();
+        File dir = new File(strPath);
+        // 该文件目录下文件全部放入数组
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                // 判断是文件还是文件夹
+                if (files[i].isDirectory()) {
+                    // 获取文件绝对路径,递归
+                    getFileList(files[i].getAbsolutePath());
+                } else {
+                    String fileName = files[i].getName();
+                    String strFileName = files[i].getAbsolutePath();
+                    map.put(fileName, strFileName);
+                }
+            }
+        }
+        return map;
+    }
+
+    // /**
+    // * 以行为单位读取文件，常用于读面向行的格式化文件
+    // */
+    // public static void readFileByLines(String fileName) {
+    // File file = new File(fileName);
+    // BufferedReader reader = null;
+    // try {
+    // System.out.println("以行为单位读取文件内容，一次读一整行：");
+    // reader = new BufferedReader(new FileReader(file));
+    // String tempString = null;
+    // int line = 1;
+    // // 一次读入一行，直到读入null为文件结束
+    // while ((tempString = reader.readLine()) != null) {
+    // // 显示行号
+    // System.out.println("line " + line + ": " + tempString);
+    // line++;
+    // }
+    // reader.close();
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // } finally {
+    // if (reader != null) {
+    // try {
+    // reader.close();
+    // } catch (IOException e1) {
+    // }
+    // }
+    // }
+    // }
+
+}
---
+++ src/main/java/com/example/demo/controller/LogsController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
+
+import com.example.demo.common.util.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ * 日志文件输出控制类
+ *
+ * @author QuiFar
+ **/
+@Controller
+@RequestMapping("/logs")
+public class LogsController {
+
+    @Value("${mylogs.Path}")
+    private String logFilePath;
+
+    /**
+     * 系统日志初始视图
+     *
+     * @return 视图名称
+     * @throws FileNotFoundException
+     */
+    @RequestMapping(path = "/list", method = {RequestMethod.GET, RequestMethod.POST})
+    public String list(Model model) throws FileNotFoundException {
+        Map<String, String> map = FileUtils.getFileNameLists(logFilePath);
+        model.addAttribute("data", map);
+        return "logs/list";
+    }
+
+    /**
+     * 系统日志翻页列表
+     *
+     * @param fileName 页面请求参数
+     * @return
+     */
+    @GetMapping("/detail/{fileName}")
+    public String get(@PathVariable("fileName") String fileName, Model model) {
+        String sfilePath = logFilePath.concat("\\").concat(fileName).concat(".log");
+        List<String> data = FileUtils.readFileByLines(sfilePath);
+        model.addAttribute("data", data);
+        return "logs/detail";
+    }
+
+}
