c1a1004ccebfdce54e1380e7e30569326a81bb65
chenkuifang <314287696@qq.com>
2018-03-29 08:42:31 +0800

    update

--- src/main/java/com/example/demo/common/Test.java
+++ src/main/java/com/example/demo/common/Test.java
@@ -10,17 +10,19 @@ public class Test {
 
 
     public static void main(String[] args) {
-        StringBuilder sql = new StringBuilder("insert into dyf_adjust_pail (id,pail_no) values ");
+        StringBuilder sql = new StringBuilder("insert into dyf_waterpail_info (pail_no,storagetype) values ");
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 200; i++) {
             StringBuilder sb = new StringBuilder();
             sb.append("(");
-            sb.append(i + 1);
+            sb.append(i + 1900001);
             sb.append(",");
-            sb.append((i + 10001));
+            sb.append(10007);
             sb.append("),");
             sql.append(sb);
         }
         System.out.println(sql);
+
+
     }
 }
